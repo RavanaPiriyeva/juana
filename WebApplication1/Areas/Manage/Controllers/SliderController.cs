5cf0a2aff141706342173c266563bf29ff766e4f
BDU Guest <bduguest@code.local>
2022-06-07 20:15:40 +0400

    image

--- WebApplication1/Areas/Manage/Controllers/SliderController.cs
+++ WebApplication1/Areas/Manage/Controllers/SliderController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.DAL;
+using WebApplication1.Helpers;
 using WebApplication1.Models;
 
 namespace WebApplication1.Areas.Manage.Controllers
@@ -12,10 +15,13 @@ namespace WebApplication1.Areas.Manage.Controllers
     public class SliderController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public SliderController(AppDbContext context)
+        public SliderController(AppDbContext context, IWebHostEnvironment env)
         {
+           
             _context = context;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -29,16 +35,34 @@ namespace WebApplication1.Areas.Manage.Controllers
         [HttpPost]
         public IActionResult Create(Slider slider)
         {
-            if (!ModelState.IsValid)
+            if (slider.ImageFile != null)
             {
-                return View();
+                if (slider.ImageFile.ContentType != "image/png" && slider.ImageFile.ContentType != "image/jpeg")
+                {
+                    ModelState.AddModelError("ImageFile", "File format must be image/png or image/jpeg");
+                }
+
+                if (slider.ImageFile.Length > 2097152)
+                {
+                    ModelState.AddModelError("ImageFile", "File size must be less than 2MB");
+                }
             }
+            else
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile is required!");
+            }
+
+            if (!ModelState.IsValid)
+                return View();
+
+            slider.Image = FileManager.Save(_env.WebRootPath, "upload/slider", slider.ImageFile);
 
             _context.Sliders.Add(slider);
             _context.SaveChanges();
 
             return RedirectToAction("index");
         }
+  
         public IActionResult Edit(int id)
         {
             Slider slider = _context.Sliders.FirstOrDefault(x => x.Id == id);
@@ -71,27 +95,19 @@ namespace WebApplication1.Areas.Manage.Controllers
 
             return RedirectToAction("index");
         }
+       
         public IActionResult Delete(int id)
-        {
-            Slider slider = _context.Sliders.FirstOrDefault(x => x.Id == id);
-
-            if (slider == null)
-                return RedirectToAction("error", "dashboard");
-
-            return View(slider);
-        }
-        
-        public IActionResult SweetDelete(int id)
         {
             Slider slider = _context.Sliders.FirstOrDefault(x => x.Id == id);
 
             if (slider == null)
                 return NotFound();
 
+            FileManager.Delete(_env.WebRootPath, "upload/slider", slider.Image);
+
             _context.Sliders.Remove(slider);
             _context.SaveChanges();
-
             return Ok();
         }
     }
-}
+}  
--- WebApplication1/Models/Slider.cs
+++ WebApplication1/Models/Slider.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,5 +25,7 @@ namespace WebApplication1.Models
         [StringLength(maximumLength: 100)]
         public string Image { get; set; }
         public int Order { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
     }
 }
